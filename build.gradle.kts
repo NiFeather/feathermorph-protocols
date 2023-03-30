a183368c8d50ac0574daa29b8652d0a80bdf06b4
MATRIX-feather <midnightcarnival@outlook.com>
2023-03-31 04:24:48 +0800

    misc: 移除重复的依赖声明
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,6 @@ publishing {
 }
 
 dependencies {
-    implementation("com.google.code.gson:gson:2.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 
