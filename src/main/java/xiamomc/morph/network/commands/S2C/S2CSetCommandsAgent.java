dd45ad197334936a21280c819512ebf7bf12a7d7
MATRIX-feather <midnightcarnival@outlook.com>
2023-03-31 16:16:27 +0800

    fix: Boolean#parseBoolean() not Boolean#getBoolean()
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- src/main/java/xiamomc/morph/network/commands/S2C/S2CSetCommandsAgent.java
+++ src/main/java/xiamomc/morph/network/commands/S2C/S2CSetCommandsAgent.java
@@ -8,13 +8,13 @@ public class S2CSetCommandsAgent extends S2CCommandWithChild<Object>
 {
     public S2CSetCommandsAgent()
     {
-        this.register(S2CCommandNames.SetAggressive, a -> new S2CSetAggressiveCommand(Boolean.getBoolean(a)))
-                .register(S2CCommandNames.SetDisplayingFakeEquip, a -> new S2CSetDisplayingFakeEquipCommand(Boolean.getBoolean(a)))
+        this.register(S2CCommandNames.SetAggressive, a -> new S2CSetAggressiveCommand(Boolean.parseBoolean(a)))
+                .register(S2CCommandNames.SetDisplayingFakeEquip, a -> new S2CSetDisplayingFakeEquipCommand(Boolean.parseBoolean(a)))
                 .register(S2CCommandNames.SetProfile, S2CSetProfileCommand::new)
                 .register(S2CCommandNames.SetSelfViewIdentifier, S2CSetSelfViewIdentifierCommand::new)
                 .register(S2CCommandNames.SetSkillCooldown, a -> new S2CSetSkillCooldownCommand(Long.parseLong(a)))
                 .register(S2CCommandNames.SetSNbt, S2CSetSNbtCommand::new)
-                .register(S2CCommandNames.SetSneaking, a -> new S2CSetSneakingCommand(Boolean.getBoolean(a)))
+                .register(S2CCommandNames.SetSneaking, a -> new S2CSetSneakingCommand(Boolean.parseBoolean(a)))
                 .register(S2CCommandNames.SetSelfViewing, a -> new S2CSetSelfViewingCommand(Boolean.valueOf(a)));
     }
 
